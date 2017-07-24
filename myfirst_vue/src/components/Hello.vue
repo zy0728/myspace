bdcf983f4a0e3f3607d299cc5310e0131116ff40
zy13163 <zy13163@ly.com>
2017-07-24 18:42:12 +0800

    update

---
+++ myfirst_vue/src/components/Hello.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>{{ message }}</h2>
+    <h2 v-bind:title="m1">title</h2>
+    <p v-if="seen">see me</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      message: 'iam',
+      m1: '页面加载于 ' + new Date(),
+      seen: true
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+  color: red;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
